de2abefc5e9cddfc622b5b489320a258a9afb431
MrWillian <willian.dev10@gmail.com>
2023-01-27 17:29:03 -0300

    feat(route): add Home Stack

---
+++ src/routes/HomeStack.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Home } from '../screens/Home';
+import { Details } from '../screens/Details';
+import { HomeStackNavigatorParamList } from './types';
+
+const HomeStack = createNativeStackNavigator<HomeStackNavigatorParamList>();
+
+export default function HomeStackNavigator() {
+  return (
+    <HomeStack.Navigator
+      initialRouteName="Home"
+      screenOptions={{ headerShown: false }}
+    >
+      <HomeStack.Screen name="Home" component={Home} />
+      <HomeStack.Screen name="Details" component={Details} />
+    </HomeStack.Navigator>
+  );
+}
