fca60f680d86184d94b0d8dc177c0e6411e22eaa
MrWillian <willian.dev10@gmail.com>
2023-01-30 16:18:20 -0300

    refactor(components): extract Title

--- src/screens/Register/styles.ts
+++ src/screens/Register/styles.ts
@@ -1,12 +1,5 @@
 import styled from 'styled-components/native';
 
-export const Title = styled.Text`
-  font-size: 20px;
-  font-weight: bold;
-  color: #8b4513;
-  margin-bottom: 10px;
-`;
-
 export const FormContainer = styled.ScrollView`
   flex: 1;
 `;
