db96d004dc4bdf75de08d2055af4dfad05208034
MrWillian <willian.dev10@gmail.com>
2023-01-30 16:19:28 -0300

    feat(routes): add Update screen route

--- src/routes/HomeStack.tsx
+++ src/routes/HomeStack.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { HomeStackNavigatorParamList } from './types';
 import { Home } from '../screens/Home';
 import { Details } from '../screens/Details';
-import { HomeStackNavigatorParamList } from './types';
 import { Register } from '../screens/Register';
+import { Update } from '../screens/Update';
 
 const HomeStack = createNativeStackNavigator<HomeStackNavigatorParamList>();
 
 export default function HomeStackNavigator() {
   return (
     <HomeStack.Navigator
-      initialRouteName="Home"
+      initialRouteName="Update"
       screenOptions={{ headerShown: false }}
     >
       <HomeStack.Screen name="Home" component={Home} />
       <HomeStack.Screen name="Details" component={Details} />
       <HomeStack.Screen name="Register" component={Register} />
+      <HomeStack.Screen name="Update" component={Update} />
     </HomeStack.Navigator>
   );
 }
