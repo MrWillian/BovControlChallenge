436a93e44d9e6d219030d1a06a80c18d67a1362d
MrWillian <willian.dev10@gmail.com>
2023-01-30 11:06:18 -0300

    feat(component): add FormButton

---
+++ src/components/FormButton/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  background-color: #8b4513;
+  border-radius: 5px;
+  elevation: 1;
+`;
+
+export const Label = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: #fff;
+`;
