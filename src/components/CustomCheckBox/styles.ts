e604b03d0acd95e74d7be1bc90a0de2ec29667f2
MrWillian <willian.dev10@gmail.com>
2023-01-30 11:24:54 -0300

    refactor(components): change checkbox bottom margin

--- src/components/CustomCheckBox/styles.ts
+++ src/components/CustomCheckBox/styles.ts
@@ -4,7 +4,7 @@ export const CheckBoxContainer = styled.View`
   display: flex;
   align-items: center;
   flex-direction: row;
-  margin: 10px 0px 20px 0px;
+  margin: 10px 0px 2px 0px;
 `;
 
 export const Label = styled.Text``;
