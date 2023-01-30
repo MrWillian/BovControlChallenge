495624d15346585327d6625e31cd3ac14c889737
MrWillian <willian.dev10@gmail.com>
2023-01-30 11:05:10 -0300

    feat(component): add ScreenContainer

---
+++ src/components/ScreenContainer/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  height: 100%;
+  align-items: center;
+  justify-content: flex-end;
+
+  background-color: #228b22;
+`;
